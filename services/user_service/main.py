d2964fd3bef8566d9cb10ecd39d099fc520bf2ff
Phucpb29 <phambaphuc1999@gmail.com>
2023-12-22 14:56:40 +0700

    Add logic litmit request api
    1. Thêm cấu hình rate limit call api
    2. Add middle ware config rate limit call api

--- services/user_service/main.py
+++ services/user_service/main.py
@@ -2,6 +2,7 @@ import time
 from fastapi import *
 from src.routers.authenticate.views import rooter as authenticate_rooter
 from src.routers.user_info.views import rooter as user_profile_rooter
+from src.middlewares.middleware_config import RateLimitingMiddleware
 
 app = FastAPI()
 
@@ -13,5 +14,6 @@ async def add_process_time_header(request: Request, call_next):
     process_time = time.time() - start_time
     response.headers["X-Process-Time"] = str(process_time)
     return response
+app.add_middleware(RateLimitingMiddleware)
 app.include_router(user_profile_rooter)
 app.include_router(authenticate_rooter)
